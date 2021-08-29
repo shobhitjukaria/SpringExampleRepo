5a2c8addd9840286b69be871aa05d541a76638f6
shobhitjukaria <shobhitjukaria@gmail.com>
2021-08-29 16:39:20 +0530

    First Commit

---
+++ springDemoApp/src/test/java/com/springDemo/springDemoApp/SpringDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springDemo.springDemoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
